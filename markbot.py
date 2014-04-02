2f1970e4188fcdfebc84f5b2c71694e8c0260d2f
Tom Hunt <tomdicksonhunt@gmail.com>
2014-04-02 11:05:01 -0600

    Latest version, now with LMDB

--- markbot.py
+++ markbot.py
@@ -2,17 +2,15 @@
 
 # A bot that does Markov-chain training on everything it hears, then spits out
 # lines on command. Maintains separate chains for each channel it joins and each
-# user it hears. 
+# user it hears.
 
 import markov
-import re, sys, signal, os
-#import cPickle as pickle
-import pickle
+import re, sys, signal, os, argparse
+import pickle as pickle
+import lmdb
 from irc.bot import SingleServerIRCBot
-
-from ffmirror import ffnet
-from io import StringIO
-from html2text import html2text
+from multiprocessing import Process, Queue
+import builtins
 
 # Utility functions for chains.
 def say_from(chain, l=250):
@@ -30,14 +28,17 @@ def say_about(chain, about, l=250):
     if not m.startswith(about):
         m = about[:-(kl-1)] + m
     if m == about:
-        m = "I don't know anything about that."
+        if about[0].islower():
+            m = say_about(chain, about[0].upper() + about[1:], l)
+        else:
+            m = "I don't know anything about that."
     return m.replace("\n", "")
 
-def learn(message, chain):
+def learn(message, chain, txn=None):
     clen = chain.clen
     l = ([''] * (clen - 2)) + list(message)
     try:
-        chain.train(l)
+        chain.train(l, txn)
     except Exception:
         pass
 
@@ -53,7 +54,7 @@ def readlogs(fn):
 
     """
     f = open(fn)
-    for i in f:
+    for n,i in enumerate(f):
         if i.startswith('---'):
             continue
         if re.match(r"^\d\d:\d\d:\d\d", i):
@@ -72,7 +73,7 @@ def readlogs(fn):
             else:
                 continue
         elif re.match(r"\d\d:\d\d", i):
-            o = re.match(r"\d\d:\d\d ( \*|< \w+>|[^ ]+) (.+)", i)
+            o = re.match(r"\d\d:\d\d ( \*|< [^ ]+>|[^ ]+) (.+)", i)
             if o is None: continue
             a = o.groups()
             if a[0] == "-!-":
@@ -84,23 +85,6 @@ def readlogs(fn):
                 who, msg = a[1].split(' ', 1)
                 msg = "a" + msg
         yield (who, msg)
-
-def read_from_file(fn):
-    with open(fn) as f:
-        for i in f:
-            yield (None, i)
-
-def read_from_fic(url):
-    n = ffnet.story_url_re.match(url).group('number')
-    md, toc = ffnet.download_metadata(n)
-    yield md['title'].replace(" ", "")
-    o = StringIO()
-    ffnet.compile_story(md, toc, o)
-    v = o.getvalue()
-    v = html2text(v)
-    v = ' '.join(v.split())
-    for i in v.split('. '):
-        yield (None, "v" + i + ".")
 
 def fold_string_indiscriminately(s, n=80):
     """Folds a string (insert line-breaks where appropriate, to format
@@ -124,7 +108,12 @@ def fold_string_indiscriminately(s, n=80):
     return '\n'.join([' '.join(i) for i in rv])
 
 class MarkovBot(SingleServerIRCBot):
-    def __init__(self, channel, nick, server, port=6667, clen=9):
+    def __init__(self, channel, nick, server, port=6667, clen=9, db=None):
+        """Arguments are straightforward. Exception: if 'db' is not None, it signifies
+        desire to use an LMDB database as backing storage instead of memory; its
+        value should be a string containing the name of the database directory.
+
+        """
         SingleServerIRCBot.__init__(self, [(server, port)], nick, nick)
         if channel:
             self.clist = [channel]
@@ -132,7 +121,25 @@ class MarkovBot(SingleServerIRCBot):
             self.clist = []
         self.clen = clen
         self.chains = {}
-        self.mlen = 480        
+        self.mlen = 480
+        if db is not None:
+            self.env = lmdb.open(db, map_size=4294967296, max_dbs=4096)
+            with self.env.begin() as txn:
+                c = txn.cursor()
+                for i in c.iternext(values=False):
+                    db = self.env.open_db(name=i)
+                    i = i.decode()
+                    self.chains[i] = markov.MarkovGenerator(self.clen, (self.env, db))
+        else:
+            self.env = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, etype, exval, etb):
+        if self.env is not None:
+            self.env.close()
+        return False
 
     def say(self, chain):
         m = say_from(chain, self.mlen)
@@ -143,17 +150,22 @@ class MarkovBot(SingleServerIRCBot):
 
     def on_nicknameinuse(self, c, e):
         on = c.get_nickname()
-        print("Nick {} in use".format(on))
+        print(("Nick {} in use".format(on)))
         c.nick(on + "_")
-        
+
     def on_welcome(self, c, e):
         for i in self.clist:
             c.join(i)
 
     def get_chain(self, arg):
         if not arg in self.chains:
-            self.chains[arg] = markov.MarkovGenerator(self.clen)
-        return self.chains[arg] 
+            if self.env is None:
+                self.chains[arg] = markov.MarkovGenerator(self.clen)
+            else:
+                argb = arg.encode() if type(arg) == str else arg
+                db = self.env.open_db(name=argb)
+                self.chains[arg] = markov.MarkovGenerator(self.clen, (self.env, db))
+        return self.chains[arg]
 
     def send_to(self, to, msg):
         l = fold_string_indiscriminately(msg, 450).split('\n')
@@ -173,21 +185,15 @@ class MarkovBot(SingleServerIRCBot):
             self.load_db(l[1])
         elif l[0] == 'logs':
             self.send_to(rnick, "Training logs (file {}, channel {})".format(l[2], l[1]))
-            self.train_from_maker(readlogs(l[2]), l[1])
+            self.train_logs(l[2], l[1])
             self.send_to(rnick, "Done")
-        elif l[0] == 'fic':
-            self.send_to(rnick, "Training fic {}".format(l[1]))
-            self.train_from_maker(read_from_fic(l[1]), l[2])
-        elif l[0] == 'file':
-            self.send_to(rnick, "Training chain {} from file {}".format(l[2], l[1]))
-            self.train_from_maker(read_from_file(l[1]), l[2])
         elif l[0] == 'join':
             self.clist.append(l[1])
             conn.join(l[1])
         elif l[0] == 'leave':
             conn.part(l[1])
         elif l[0] == 'chains':
-            self.send_to(rnick, ', '.join(self.chains.keys()))
+            self.send_to(rnick, ', '.join(list(self.chains.keys())))
             if len(self.chains) == 0:
                 self.send_to(rnick, "No chains")
         elif l[0] == 'version':
@@ -206,7 +212,7 @@ class MarkovBot(SingleServerIRCBot):
             m = re.search("[Tt]alk like ([#\w-]+)", message)
             if m:
                 c = self.get_chain(m.group(1))
-            m = re.search("[Aa]bout (\w+)[.?!]?\s*$", message)
+            m = re.search("[Aa]bout (.+?)[.?!]?\s*$", message)
             if m:
                 conn.privmsg(target, say_about(c, m.group(1)))
                 return
@@ -226,16 +232,10 @@ class MarkovBot(SingleServerIRCBot):
         c = self.get_chain(channel)
         for i in readlogs(infile):
             learn(i[1], c)
+            if len(i[0]) == 0:
+                continue
             uc = self.get_chain(i[0])
             learn(i[1], uc)
-
-    def train_from_maker(self, gen, channel):
-        c = self.get_chain(channel)
-        for i in gen:
-            learn(i[1], c)
-            if i[0]:
-                uc = self.get_chain(i[0])
-                learn(i[1], uc)
 
     def on_action(self, conn, ev):
         message = ev.arguments[0]
@@ -249,52 +249,101 @@ class MarkovBot(SingleServerIRCBot):
         if nick.lower() in message.lower():
             conn.privmsg(target, self.say(c))
 
-def main():
-    bot, server, port, nick = None, None, None, None
-    def sigterm(sig, thing):
-        p = os.getpid()
-        fn = "mbdump-{}".format(p)
-        bot.dump_db(fn)
-        d = (server, port, nick, fn)
-        with open('markbot-{}'.format(p), 'wb') as o:
-            pickle.dump(d, o, -1)
-        if sig != None:
-            os._exit(1)
-    if len(sys.argv) < 2:
-        print("Usage: {} <server[:port]> <nick> [<channel>]".format(sys.argv[0]))
-        sys.exit(1)
-    if len(sys.argv) < 3:
-        if os.path.exists(sys.argv[1]):
-            with open(sys.argv[1], 'rb') as i:
-                server, port, nick, fn = pickle.load(i)
-            bot = MarkovBot(None, nick, server, port)
-            bot.load_db(fn)
-            try:
-                bot.start()
-            except Exception as e:
-                sigterm(None, None)
-                raise e
+def do_training(source, chain, db, clen=9):
+    """Take a source (an iterator over lines) and a chain (name of chain to train
+    into), and train the lines into the chain. If the source yields tuples, they
+    will be assumed to be 2-tuples (chain, line), and each line will be trained
+    into both the chain named as an argument and the chain in the tuple.
+
+    """
+    def write_items(db, q, clen):
+        env = lmdb.open(db, map_size=4294967296, max_dbs=4096)
+        chains = {}
+        with env.begin() as txn:
+            c = txn.cursor()
+            for i in c.iternext(values=False):
+                db = env.open_db(name=i)
+                i = i.decode()
+                chains[i] = markov.MarkovGenerator(clen, (env, db))
+
+        lines = {}
+        names = []
+
+        def get_chain(arg):
+            if not arg in chains:
+                argb = arg.encode() if type(arg) == str else arg
+                db = env.open_db(name=argb)
+                chains[arg] = markov.MarkovGenerator(clen, (env, db))
+            return chains[arg]
+
+        for i in iter(q.get, None):
+            if len(i[0]) == 0:
+                print("skipped {}".format(i))
+                continue
+            if not i[0] in lines:
+                lines[i[0]] = []
+                if not i[0] in names:
+                    names.append(i[0])
+                    print(i[0])
+            lines[i[0]].append(i[1])
+            if len(lines[i[0]]) >= 800:
+                c = get_chain(i[0])
+                with env.begin(write=True, db=c.db) as txn:
+                    for j in lines[i[0]]:
+                        learn(j, c, txn)
+                del lines[i[0]]
+
+        for i in lines.keys():
+            print("Finishing {}".format(i))
+            c = get_chain(i)
+            with env.begin(write=True, db=c.db) as txn:
+                for j in lines[i]:
+                    learn(j, c, txn)
+
+    q = Queue(10000)
+    p = Process(target=write_items, args=(db,q,clen))
+    p.start()
+
+    for n,i in enumerate(source):
+        if type(i) == builtins.tuple:
+            q.put(i)
+            q.put((chain, i[1]))
         else:
-            print("Usage: {} <server[:port]> <nick> [<channel>]".format(sys.argv[0]))
+            q.put((chain, i))
+        print(n, end='\r')
+    print('', end='\n')
+
+    q.put(None)
+    q.close()
+    p.join()
+
+def main():
+    parser = argparse.ArgumentParser(description="IRC bot that trains Markov chains and runs their output on demand")
+    parser.add_argument("server", help="IRC server to connect")
+    parser.add_argument("nick", help="Nickname to use")
+    parser.add_argument("-p", "--port", type=int, help="Port to connect to", default=6667)
+    parser.add_argument("-d", "--database-file", help="Database filename to use; if unspecified, no database", default=None)
+    parser.add_argument("-c", "--channel-join", help="Channel to join on startup", default=None)
+    parser.add_argument("-t", "--train-file", help="Train database (faster than online)", default=None)
+    parser.add_argument("-l", "--train-logs", action="store_true", help="Train a logs file", default=False)
+    parser.add_argument("-r", "--train-chain", help="Chain to train into", default=None)
+    a = parser.parse_args()
+
+    if a.train_file is not None:
+        if not a.train_chain:
+            print("Error: need chain", file=sys.stderr)
             sys.exit(1)
-    s = sys.argv[1].split(':', 1)
-    server = s[0]
-    try:
-        port = int(s[1])
-    except IndexError:
-        port = 6667
-    try:
-        channel = sys.argv[3]
-    except IndexError:
-        channel = None
-    nick = sys.argv[2]
-    bot = MarkovBot(channel, nick, server, port)
-    signal.signal(signal.SIGTERM, sigterm)
-    try:
-        bot.start()
-    except Exception as e:
-        sigterm(None, None)
-        raise e
+        if not a.database_file:
+            print("Error: need DB", file=sys.stderr)
+            sys.exit(1)
+        if a.train_logs:
+            it = readlogs(a.train_file)
+        else:
+            it = open(a.train_file, 'r')
+        do_training(it, a.train_chain, a.database_file)
+    else:
+        with MarkovBot(a.channel_join, a.nick, a.server, a.port, db=a.database_file) as bot:
+            bot.start()
 
 if __name__=="__main__":
     main()
--- markov.py
+++ markov.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 import random, re, sys
-import lmdb
+from collections import Counter
+try:
+    import json, lmdb
+except ImportError:
+    json = lmdb = None
 
 toks = [] # global token deduplicator
 
@@ -15,28 +19,31 @@ def dedup(tok):
     else:
         return toks[n]
 
-class PersistentDict(object):
-    """A dictionary, made persistent via lmdb. Since lmdb uses memory-mapped files,
-    also reduces memory load of large dicts. Supports only limited numbers of
-    objects as keys/values, since lmdb requires only strings.
-
-    """
-    def __init__(self, env, db):
-        self.env = env
-        self.db = db
-        
-
 class MarkovGenerator:
-    def __init__(self, clen=3):
+    def __init__(self, clen=3, db=None):
         """Initialize the generator for a given chain length. The longer the chain
-        length, the more similar generated output will be to training
-        input. Reasonable values vary; for word-based text generation on
-        relatively short inputs, 3-5 is good, though 5 may end up generating
-        mostly direct quotes. Letter-based generation will need larger values.
+        length, the more similar generated output will be to training input.
+        Reasonable values vary; for word-based text generation on relatively
+        short inputs, 3-5 is good, though 5 may end up generating mostly direct
+        quotes. Letter-based generation will need larger values.
+
+        This object can use an LMDB database, if passed it. If desired, 'db'
+        should be a tuple of (env, db), where these are the result of
+        lmdb.open() and env.open_db() respectively. (MarkovGenerator does not
+        want or need to care about filenames, named databases, &c.; it uses the
+        handles passed. Actually doing LMDB handling is left to the caller.) The
+        passed-in DB will be arbitrarily scribbled-on by the writing of keys; it
+        really does need a dedicated one per generator.
 
         """
         self.clen = clen
-        self.db = {}
+        if db is None:
+            self.lmdb = False
+            self.db = {}
+        else:
+            self.lmdb = True
+            self.env = db[0]
+            self.db = db[1]
 
     def ntuples(self, ilist):
         if len(ilist) < self.clen:
@@ -45,19 +52,67 @@ class MarkovGenerator:
             r = tuple([dedup(n) for n in ilist[i:i+self.clen]])
             yield r
 
-    def train(self, ilist):
+    def train_token(self, k, v, txn=None):
+        if not self.lmdb:
+            if k in self.db:
+                self.db[k].append(v)
+            else:
+                self.db[k] = [v]
+        else:
+            ks = json.dumps(k, separators=(',',':')).encode()
+            vd = txn.get(ks, default=b"{}")
+            vs = json.loads(vd.decode())
+            if v in vs:
+                vs[v] += 1
+            else:
+                vs[v] = 1
+            vsn = json.dumps(vs, separators=(',',':')).encode()
+            txn.put(ks, vsn)
+
+    def train(self, ilist, txn=None):
         """Feed the generator input to train on. ilist is an arbitrary list of data,
         probably strings or integers. Common use cases are lists of the words or
         letters of a text file, in order to generate text with a resemblance to
         that file.
 
         """
-        for i in self.ntuples(ilist):
-            k, v = i[:-1], i[-1]
-            if k in self.db:
-                self.db[k].append(v)
-            else:
-                self.db[k] = [v]
+        if self.lmdb:
+            made = False
+            if txn is None:
+                made = True
+                txn = self.env.begin(write=True, db=self.db)
+            for i in self.ntuples(ilist):
+                k, v = i[:-1], i[-1]
+                self.train_token(k, v, txn)
+            if made:
+                txn.commit()
+        else:
+            for i in self.ntuples(ilist):
+                k, v = i[:-1], i[-1]
+                self.train_token(k, v)
+
+    def get_statelist(self):
+        if not self.lmdb:
+            return list(self.db.keys())
+        else:
+            with self.env.begin(db=self.db) as txn:
+                cursor = txn.cursor()
+                i = cursor.iternext(values=False)
+                return list(i)
+
+    def get_state_toks(self, state, txn=None):
+        if not self.lmdb:
+            return self.db[state]
+        else:
+            ks = json.dumps(state, separators=(',',':'))
+            v = txn.get(ks, default=b"{}")
+            vs = json.loads(v.decode())
+            if not vs:
+                raise KeyError("Not in database")
+            rv = []
+            for v in vs:
+                rv.extend([v] * vs[v])
+            return rv
 
     def generate(self, n, istate=None):
         """Generate output. The result is a generator that will yield at most n
@@ -70,15 +125,19 @@ class MarkovGenerator:
         if n < self.clen:
             raise Exception("Not long enough")
         if istate == None:
-            state = random.choice(list(self.db.keys()))
+            state = random.choice(self.get_statelist())
         else:
             state = istate
         for i in state:
             yield i
         n -= len(state)
+        if self.lmdb:
+            txn = self.env.begin(db=self.db)
+        else:
+            txn = None
         for i in range(n):
             try:
-                w = random.choice(self.db[state])
+                w = random.choice(self.get_state_toks(state, txn))
             except KeyError:
                 break
             state = state[1:] + (w,)
